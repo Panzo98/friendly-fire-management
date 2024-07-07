b1ae06358dec2fa8e3a9661ba392011742bc8257
panzo <administrator@admins-MacBook-Air.local>
2024-07-07 14:57:41 +0200

    Postavljanje strukture projekta

---
+++ screens/LoginScreen.jsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
